34cd776f1ada186304a627dba211299c422e8cf3
Bruno Maass <brunomaass@yahoo.com.br>
2024-05-21 21:17:40 -0400

    Finalizar exercicios pagina 78.

---
+++ capitulo3/exercicios/HealthProfile.java
@@ -0,0 +1,167 @@
+package capitulo3.exercicios;
+
+public class HealthProfile
+{
+    private String name;
+    private String lastName;
+    private String sex;
+    private int yearBirth;
+    private int monthBirth;
+    private int dayBirth;
+    private double height;
+    private int weight;
+
+    // Contrutor
+    public HealthProfile(String name, String lastName, String sex, int yearBirth, int monthBirth, int dayBirth,
+                         int height, int weight)
+    {
+        this.name = name;
+        this.lastName = lastName;
+        this.sex = sex;
+        this.yearBirth =yearBirth;
+        this.monthBirth = monthBirth;
+        this.dayBirth = dayBirth;
+        this.height = height;
+        this.weight = weight;
+    }
+
+
+    // sets and gets -- name
+    public void setName(String name)
+    {
+        this.name = name;
+    }
+
+
+    public String getName()
+    {
+        return name;
+    }
+
+
+    // set and get -- lastName
+    public void setLastName(String lastName)
+    {
+        this.lastName = lastName;
+    }
+
+
+    public String getLastName()
+    {
+        return lastName;
+    }
+
+
+    // set and get -- sex
+    public void setSex(String sex)
+    {
+        this.sex = sex;
+    }
+
+
+    public String getSex()
+    {
+        return sex;
+    }
+
+
+    // set and get -- yearBirth
+    public void setYearBirth(int yearBirth)
+    {
+        this.yearBirth = yearBirth;
+    }
+
+
+    public int getYearBirth()
+    {
+        return yearBirth;
+    }
+
+
+    // set and get -- monthBirth
+    public void setMonthBirth(int monthBirth)
+    {
+        this.monthBirth = monthBirth;
+    }
+
+
+    public int getMonthBirth()
+    {
+        return monthBirth;
+    }
+
+
+    // set and get -- dayBirth
+    public void setDayBirth(int dayBirth)
+    {
+        this.dayBirth = dayBirth;
+    }
+
+
+    public int getDayBirth()
+    {
+        return dayBirth;
+    }
+
+
+    // set and get -- height
+    public void setHeight(double height)
+    {
+        this.height = height;
+    }
+
+
+    public double getHeight()
+    {
+        return height;
+    }
+
+
+    // set and get -- weight
+    public void setWeight(int weight)
+    {
+        this.weight = weight;
+    }
+
+
+    public int getWeight()
+    {
+        return weight;
+    }
+
+
+    // Calcula idade em anos
+    public int calcAge()
+    {
+
+        return (this.yearBirth - 2024) * -1;
+    }
+
+
+    // calcula a frequencia cardíaca
+    public int calcMaxHeathFrequency()
+    {
+        return 220 - calcAge();
+    }
+
+
+    // caclcula o valor mínimo do intervalo da frequencia ideal
+    public double minIdealFrequency()
+    {
+        return calcMaxHeathFrequency() * 0.5;
+    }
+
+
+    // calcula o valor maximo do intervalo da frequencia cardiaca ideal.
+    public double maxIdealFrequency()
+    {
+        return calcMaxHeathFrequency() * 0.85;
+    }
+
+
+    // Calcula IMC
+    public double calcImc()
+    {
+        return weight / (height * height);
+    }
+}
---
+++ capitulo3/exercicios/Inicial.java
@@ -0,0 +1,46 @@
+package capitulo3.exercicios;
+import java.util.Scanner;
+
+public class Inicial {
+    public static void main(String[] args) {
+        HealthProfile profile = new HealthProfile("", "", "", 0, 0,
+                0, 0, 0);
+
+        // Iniciando scanner
+        Scanner input = new Scanner(System.in);
+
+        // Entrando com os dados do usuário
+        System.out.println("Digite seu primeiro nome:");
+        profile.setName(input.nextLine());
+
+        System.out.println("Digite se sobrenome:");
+        profile.setLastName(input.nextLine());
+
+        System.out.println("Digite seu sexo (masculino ou feminino):");
+        profile.setSex(input.nextLine());
+
+        System.out.println("Digite o dia do seu nascimento: ");
+        profile.setDayBirth(input.nextInt());
+
+        System.out.println("Digite o mes do seu nascimento: ");
+        profile.setMonthBirth(input.nextInt());
+
+        System.out.println("Digite o ano do seu nascimento: ");
+        profile.setYearBirth(input.nextInt());
+
+        System.out.println("Digite a sua altura em metros:");
+        profile.setHeight(input.nextDouble());
+
+        System.out.println("Digite o seu peso em quilos: ");
+        profile.setWeight(input.nextInt());
+
+        // Saida dos dados
+        System.out.printf("%nNome completo: %s %s%n", profile.getName(), profile.getLastName());
+        System.out.printf("Idade em anos: %d%n", profile.calcAge());
+        System.out.printf("Sexo: %s%n", profile.getSex());
+        System.out.printf("Sua frequência cardíaca máxima é: %d%n", profile.calcMaxHeathFrequency());
+        System.out.printf("Sua frequência cardiaca ideal é entre: %.2f e %.2f%n", profile.minIdealFrequency(), profile.maxIdealFrequency());
+        System.out.printf("Seu IMC é %.2f", profile.calcImc());
+
+    }
+}
